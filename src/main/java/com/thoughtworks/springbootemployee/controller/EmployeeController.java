02bb1e892fb12778f627e59d1181a65359b2c863
wongro2 <roy.wong@oocl.com>
2020-04-15 20:23:24 +0800

    [Roy] updated to show specific employee api

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -7,35 +7,68 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
     private List<Employee> employees = new ArrayList<>();
 
+    public EmployeeController(List<Employee> employees) {
+        this.employees = employees;
+    }
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Employee> getAllEmployees() {
-        employees.add(new Employee(0,"Xiaoming",20,"Male"));
-        employees.add(new Employee(1,"Xiaohong",19,"Female"));
-        employees.add(new Employee(2,"Xiaozhi",15,"Male"));
-        employees.add(new Employee(3,"Xiaogang",16,"Male"));
-        employees.add(new Employee(4,"Xiaoxia",16,"Female"));
+        employees.add(new Employee(0, "Xiaoming", 20, "Male"));
+        employees.add(new Employee(1, "Xiaohong", 19, "Female"));
+        employees.add(new Employee(2, "Xiaozhi", 15, "Male"));
+        employees.add(new Employee(3, "Xiaogang", 16, "Male"));
+        employees.add(new Employee(4, "Xiaoxia", 16, "Female"));
         return employees;
     }
 
-    @GetMapping("/employeeID")
-    @ResponseStatus(HttpStatus.OK)
-    public Employee getAnEmployee(int employeeID) {
-        return (Employee) employees.stream().filter(employee -> employee.getEmployeeID().equals(employeeID));
+    @GetMapping("/{employeeID}")
+    public Employee getAnEmployee(@PathVariable int employeeID) {
+        employees.add(new Employee(0, "Xiaoming", 20, "Male"));
+        for (Employee employee : employees) {
+            if (employee.getEmployeeID() == employeeID) {
+                return employee;
+            }
+        }
+        return null;
     }
+    /*
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Employee> getEmployeeWithPageQuery(@RequestParam int page, int pageSize) {
+        employees.add(new Employee(0, "Xiaoming", 20, "Male"));
+        employees.add(new Employee(1, "Xiaohong", 19, "Female"));
+        employees.add(new Employee(2, "Xiaozhi", 15, "Male"));
+        employees.add(new Employee(3, "Xiaogang", 16, "Male"));
+        employees.add(new Employee(4, "Xiaoxia", 16, "Female"));
+        return employees;
+    }*/
 
     @PostMapping
+    @ResponseStatus(HttpStatus.OK)
     public Employee createNewEmployee(@RequestBody Employee employee) {
         employees.add(employee);
         return employee;
     }
 
+    @PutMapping("/{employeeId}")
+    public Employee updateNewEmployee(int employeeID, Employee newEmployee) {
+        employees.remove(employeeID); //replace old employee
+        employees.add(newEmployee);
+        return newEmployee;
+    }
 
+    @DeleteMapping("/{employeeId}")
+    public void deleteNewEmployee(int employeeID) {
+        employees.remove(employeeID);
+        //return employee;
+    }
 
 
 }
